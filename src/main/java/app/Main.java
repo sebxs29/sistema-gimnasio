de22dc98446a61a4086f46146e81397bae04f9b8
sebxs29 <sebas29t@gmail.com>
2026-06-24 17:18:33 -0500

    Agrega dependencias, conexión PostgreSQL, script SQL y Main funcional

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,21 @@
 package app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("LOGIN");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main (String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/db/Conexion.java
+++ src/main/java/db/Conexion.java
@@ -1,4 +1,15 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static final String URL = "jdbc:postgresql://localhost:5432/gimnasio_db";
+    private static final String USUARIO = "postgres";
+    private static final String CONTRASENA = "123456";
+
+    public static Connection getConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+    }
 }
