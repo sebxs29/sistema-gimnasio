8632c813ebf972fb7b8c6ad03e28bf33e2df4c58
TheErickYT10 <erickpatino777@gmail.com>
2026-07-05 14:43:50 -0500

    Clase Entrenador Controller

--- src/main/java/controller/EntrenadorController.java
+++ src/main/java/controller/EntrenadorController.java
@@ -1,4 +1,89 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class EntrenadorController {
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private ComboBox<String> cbEspecialidad;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtContrasena;
+
+    @FXML
+    private TableView<?> tablaEntrenadores;
+
+    @FXML
+    private TableColumn<?, ?> colCedula;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colApellido;
+
+    @FXML
+    private TableColumn<?, ?> colEspecialidad;
+
+    @FXML
+    private TableColumn<?, ?> colTelefono;
+
+    @FXML
+    private TableColumn<?, ?> colUsuario;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    private void guardar() {
+
+    }
+
+    @FXML
+    private void actualizar() {
+
+    }
+
+    @FXML
+    private void eliminar() {
+
+    }
+
+    @FXML
+    private void limpiar() {
+
+    }
+
+    @FXML
+    private void seleccionarEntrenador() {
+
+    }
+
+    private void cargarEntrenadores() {
+
+    }
+
+    private boolean validarCampos() {
+        return false;
+    }
+
+
 }
