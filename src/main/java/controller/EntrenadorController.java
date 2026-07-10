8c9d3c8a9b06e7a293dafde832bd1efcab98fff0
TheErickYT10 <erickpatino777@gmail.com>
2026-07-10 12:15:06 -0500

    Guardo mis cambios

--- src/main/java/controller/EntrenadorController.java
+++ src/main/java/controller/EntrenadorController.java
@@ -2,54 +2,47 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Entrenador;
 
 public class EntrenadorController {
 
-    @FXML
-    private TextField txtCedula;
+    @FXML private TextField txtCedula;
 
-    @FXML
-    private TextField txtNombre;
+    @FXML private TextField txtNombre;
 
-    @FXML
-    private TextField txtApellido;
+    @FXML private TextField txtApellido;
 
-    @FXML
-    private ComboBox<String> cbEspecialidad;
+    @FXML private ComboBox<String> cbEspecialidad;
 
-    @FXML
-    private TextField txtTelefono;
+    @FXML private TextField txtTelefono;
 
-    @FXML
-    private TextField txtUsuario;
+    @FXML private TextField txtUsuario;
 
-    @FXML
-    private PasswordField txtContrasena;
+    @FXML private PasswordField txtContrasena;
 
-    @FXML
-    private TableView<?> tablaEntrenadores;
+    @FXML private TableView<Entrenador> tablaEntrenadores;
 
-    @FXML
-    private TableColumn<?, ?> colCedula;
+    @FXML private TableColumn<Entrenador, String> colCedula;
 
-    @FXML
-    private TableColumn<?, ?> colNombre;
+    @FXML private TableColumn<Entrenador, String> colNombre;
 
-    @FXML
-    private TableColumn<?, ?> colApellido;
+    @FXML private TableColumn<Entrenador, String> colApellido;
 
-    @FXML
-    private TableColumn<?, ?> colEspecialidad;
+    @FXML private TableColumn<Entrenador, String> colEspecialidad;
 
-    @FXML
-    private TableColumn<?, ?> colTelefono;
-
-    @FXML
-    private TableColumn<?, ?> colUsuario;
+    @FXML private TableColumn<String, String> colTelefono;
 
     @FXML
     public void initialize() {
-
+        cbEspecialidad.getItems().addAll("Preparador Físico", "Director Técnico", "Entrenador de Porteros", "Asistente Técnico");
+        colCedula.setCellValueFactory(new PropertyValueFactory<>("cedula"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colApellido.setCellValueFactory(new PropertyValueFactory<>("apellido"));
+        colEspecialidad.setCellValueFactory(new PropertyValueFactory<>("especialidad"));
+        colTelefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+        cargarEntrenadores();
+        tablaEntrenadores.setOnMouseClicked(e -> seleccionarEntrenador());
     }
 
     @FXML
